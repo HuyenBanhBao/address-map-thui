f86d26a1d79a78564a209ebec79bc804e0cf53ef
HuyenBanhBao <ngoctung2307th@gmail.com>
2026-08-13 11:29:28 +0700

    fix GitHub Pages base path

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  // GitHub Pages publishes this project under /address-map-thui/.
+  base: '/address-map-thui/',
   plugins: [react()],
 })
